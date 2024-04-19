68fd57e26a9a30cfe2e61df73ec6ac7670429795
achigy <gonzalesalison775@gmail.com>
2024-04-18 21:08:21 -0400

    SlideBar mantiene iluminado la ruta en la que se encuentra

--- frontend/src/components/ui/SlideBar.tsx
+++ frontend/src/components/ui/SlideBar.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SlideBar.css';
 import { slideBarAdmin } from '../../lib/constants.tsx';
 import {Link, useLocation } from "react-router-dom"
 export const SlideBar = () => { 
-  useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  const handleMenuToggle = () => {
+    setMenuOpen(true);
+  };
   return (
     <aside className="menu-lateral">
         <div>
@@ -15,14 +20,16 @@ export const SlideBar = () => {
             <li key={ option.path }>
               <Link to={{ pathname: option.path}}
                 style={{ textDecoration: 'none', color: 'inherit' }}>
-                <button className={`itemMenu${!(option.path).includes(option.path) && 'active'}`}>
+                <button 
+                  className={`itemMenu${(location.pathname).includes(option.path) && 'active'}`}
+                  onMouseOver={handleMenuToggle}>
                 <span style={{ marginRight: '20px' }}>{option.icon}</span>
                 {option.name}
                 </button>
               </Link>
             </li>
-          ))
-         }
+           ))
+          }
         </ul>
       </aside>
   )
