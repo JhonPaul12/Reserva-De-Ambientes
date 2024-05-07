a2b685c9eb0fd0497e6dee399fd94a55750371cb
Hualca Yavi Orlando <zeth20001@gmail.com>
2024-05-06 23:24:46 -0400

    materia-modal

--- frontend/src/CancelarSolicitud/components/CancelarS.tsx
+++ frontend/src/CancelarSolicitud/components/CancelarS.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Button,
   Table,
@@ -6,70 +7,63 @@ import {
   TableColumn,
   TableHeader,
   TableRow,
+  Modal,
+  ModalContent,
 } from "@nextui-org/react";
 import axios from "axios";
-import { useEffect, useState } from "react";
-
-export interface Solicitud {
-  id: number;
-  motivo: string;
-  fecha_solicitud: string;
-  hora_inicio: string;
-  hora_fin: string;
-  estado: string;
-  numero_estudiantes: number;
-  ambiente_id: number;
-  created_at: string;
-  updated_at: string;
-  pivot: {
-    user_id: number;
-    solicitud_id: number;
-  };
-  ambiente: {
-    id: number;
-    nombre: string;
-    tipo: string;
-    ubicacion: string;
-    capacidad: number;
-  };
-  docente: {
-    id: number;
-    nombre: string;
-    apellido: string;
-  };
-}
+import { Solicitud } from "../interfaces/Solicitud";
+import "./estilos.css"
 
 export const CancelarS = () => {
   const [solicitudes, setSolicitudes] = useState<Solicitud[]>([]);
-  
-    useEffect(()=>{
-      getSolicitudes();
-    }, []);
-  
-    const getSolicitudes = async () => {
-      const respuesta = await axios.get<Solicitud[]>(`http://127.0.0.1:8000/api/showAllDocentes/Vladimir Abel`);
-      const solicitudesPendientes = respuesta.data.filter(solicitud => solicitud.estado === "Pendiente");
-      setSolicitudes(solicitudesPendientes);
-    }
+  const [modalOpen, setModalOpen] = useState(false);
+  const [solicitudIdToDelete, setSolicitudIdToDelete] = useState<number | null>(null);
 
-    const handleCancelarSolicitud = async (solicitudId: number) => {
+  useEffect(() => {
+    getSolicitudes();
+  }, []);
+
+  const getSolicitudes = async () => {
+    const respuesta = await axios.get<Solicitud[]>(
+      `http://127.0.0.1:8000/api/showAllDocentes/Vladimir Abel`
+    );
+    const solicitudesPendientes = respuesta.data.filter(
+      (solicitud) => solicitud.estado === "Pendiente"
+    );
+    setSolicitudes(solicitudesPendientes);
+  };
+
+  const handleCancelarSolicitud = async (solicitudId: number) => {
+    setSolicitudIdToDelete(solicitudId);
+    setModalOpen(true);
+  };
+
+  const confirmarCancelarSolicitud = async () => {
+    if (solicitudIdToDelete) {
       try {
-        await axios.delete(`http://127.0.0.1:8000/api/solicitud/${solicitudId}`);
-        const nuevasSolicitudes = solicitudes.filter(solicitud => solicitud.id !== solicitudId);
+        await axios.delete(`http://127.0.0.1:8000/api/solicitud/${solicitudIdToDelete}`);
+        const nuevasSolicitudes = solicitudes.filter(
+          (solicitud) => solicitud.id !== solicitudIdToDelete
+        );
         setSolicitudes(nuevasSolicitudes);
+        setModalOpen(false);
       } catch (error) {
         console.error("Error al cancelar la solicitud:", error);
       }
     }
+  };
 
   return (
     <div className="contenedor-table">
-     <label className='ml-10 text-3xl font-bold text-center text-gray-900'>CANCELAR SOLICITUD</label>
+      <label className="ml-10 text-3xl font-bold text-center text-gray-900">
+        CANCELAR SOLICITUD
+      </label>
       <section className="mx-6 my-4">
         <Table className="custom-table" aria-label="Tabla de datos">
           <TableHeader>
-            <TableColumn className="text-center text-3xl bg-slate-300">Id</TableColumn>
             <TableColumn className="text-center text-3xl bg-slate-300">Ambiente</TableColumn>
+            <TableColumn className="text-center text-3xl bg-slate-300">Docente</TableColumn>
+            <TableColumn className="text-center text-3xl bg-slate-300">Materia</TableColumn>
             <TableColumn className="text-center text-3xl bg-slate-300">H. Inicio</TableColumn>
             <TableColumn className="text-center text-3xl bg-slate-300">H. Final</TableColumn>
             <TableColumn className="text-center text-3xl bg-slate-300">Fecha</TableColumn>
@@ -81,10 +75,13 @@ export const CancelarS = () => {
             {solicitudes.map((solicitud) => (
               <TableRow key={solicitud.id}>
                 <TableCell className="text-base text-black">
-                  {solicitud.id}
+                  {solicitud.ambiente.nombre}
                 </TableCell>
                 <TableCell className="text-base text-black">
-                  {solicitud.ambiente.nombre}
+                  {solicitud.docente.nombre+" "+solicitud.docente.apellido}
+                </TableCell>
+                <TableCell className="text-base text-black">
+                  {solicitud.materia.nombre_materia}
                 </TableCell>
                 <TableCell className="text-base text-black">
                   {solicitud.hora_inicio}
@@ -102,13 +99,29 @@ export const CancelarS = () => {
                   {solicitud.estado}
                 </TableCell>
                 <TableCell className="text-base text-black">
-                  <Button className="bg-danger" onClick={() => handleCancelarSolicitud(solicitud.id)}>Cancelar</Button>
+                  <Button
+                    className="bg-danger"
+                    onClick={() => handleCancelarSolicitud(solicitud.id)}
+                  >
+                    Cancelar
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </section>
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)} className="p-10 bg-white">
+        <ModalContent className="">
+          ¿Estás seguro de que quieres cancelar esta solicitud?
+          <Button className="bg-danger m-2 text-white" onClick={confirmarCancelarSolicitud}>
+            Sí, cancelar
+          </Button>
+          <Button className="m-2" onClick={() => setModalOpen(false)}>
+            No
+          </Button>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
--- frontend/src/CancelarSolicitud/page/CancelarSol.tsx
+++ frontend/src/CancelarSolicitud/page/CancelarSol.tsx
@@ -2,8 +2,6 @@ import { CancelarS } from "../components/CancelarS";
 
 export const CancelarSol = () => {
   return (
-    <div className="mt-10 sm:mx-auto sm:w-full">
     <CancelarS />
-    </div>
   );
 };
