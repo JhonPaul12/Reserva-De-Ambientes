3c9b08cdef2f629302af2143556c90ecc98b40dd
Hualca Yavi Orlando <zeth20001@gmail.com>
2024-05-07 22:31:16 -0400

    filtros-modificados

--- frontend/src/BusquedaFiltros/interfaces/Ambiente.ts
+++ frontend/src/BusquedaFiltros/interfaces/Ambiente.ts
@@ -1,21 +1,21 @@
-export interface Ambiente {
-    id: number;
-    nombre: string;
-    capacidad: number;
-    tipo: string;
-    ubicacion: string;
-}
+
 export interface Periodo {
     id: number;
     id_ambiente: number;
     id_horario: number;
     estado: string;
     fecha: string;
+    ambiente: {
+      id:number,
+      nombre:string,
+      tipo:string,
+      ubicacion:string,
+      capacidad:number
+    }
+    horario:{
+      id:number,
+      dia:string,
+      hora_inicio:string,
+      hora_fin:string
+    }
   }
-export interface Horario {
-    id: number;
-    dia: number;
-    hora_inicio: string;
-    hora_fin: string;
-  }
-  
