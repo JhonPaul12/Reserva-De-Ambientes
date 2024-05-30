3dcdecc8bd25c10c57d717f6cd662ae354a06098
andres7o7 <andresrevollohuanca@gmail.com>
2024-05-29 22:44:05 -0400

    relacion users rols

--- backend/database/migrations/2024_04_20_001535_create_rol_user_table.php
+++ backend/database/migrations/2024_04_20_001535_create_rol_user_table.php
@@ -15,7 +15,7 @@ class CreateRolUserTable extends Migration
     {
         Schema::create('rol_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('user')->onDelete('cascade'); 
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
             $table->foreignId('rol_id')->constrained('rols')->onDelete('cascade');
         });
     } 
